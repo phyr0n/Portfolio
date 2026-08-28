ebc96dc50cc9599c5893871431eed7f1d9b5aaad
Adam Tait <adamtaitcs@gmail.com>
2026-08-28 21:48:05 +0100

    Include missing file from commit
    Turns out I didn't save it, oops...

--- src/features/home/components/home-route.tsx
+++ src/features/home/components/home-route.tsx
@@ -1,5 +1,4 @@
 import { HeroSection } from './hero-section';
-import { FocusSection } from './focus-section';
 import { FeaturedProjectsSection } from './featured-projects-section';
 
 export const HomeRoute = () => {
